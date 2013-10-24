b904b486b73633d00533dacf198ca54b6d86ac70
Priscilla <priscilla_hasya@windowslive.com>
2013-10-24 15:58:44 -0200

    adicionando regras e fatos

--- previsaodotempo.pl
+++ previsaodotempo.pl
@@ -3,19 +3,20 @@
 :- dynamic tipoclima /2.
 :- dynamic tempo/2.
 :- dynamic condicao_clima_para_lugar /2.
-:- dynamic temperaturas_cidade/3.
+:- dynamic temperatura_minima/1.
+:- dynamic temperatura_maxima/1.
 
-clima(t,95,18,25,12,macapa).
+clima(t,95,18,24,12,macapa).
 clima(s,67,12,22,13,belem).
-clima(n,56,13,20,14,aracaju).
+clima(n,56,10,20,14,aracaju).
 clima(c,78,16,26,15,salvador).
-clima(e,68,19,22,16,curitiba).
+clima(e,68,14,18,16,curitiba).
 
-clima_sem_umidade(n,18,25,12,macapa).
+clima_sem_umidade(n,18,24,12,macapa).
 clima_sem_umidade(c,12,22,13,belem).
-clima_sem_umidade(t,13,20,14,aracaju).
+clima_sem_umidade(t,10,20,14,aracaju).
 clima_sem_umidade(s,16,26,15,salvador).
-clima_sem_umidade(e,19,22,16,curitiba).
+clima_sem_umidade(e,14,18,16,curitiba).
 
 condicao_clima_para_lugar(t,macapa).
 condicao_clima_para_lugar(s,belem).
@@ -35,9 +36,17 @@ tipoclima(t,tempestade).
 tipoclima(s,sol).
 tipoclima(e,encoberto).
 
-temperaturas_cidade(18,25,macapa).
-temperaturas_cidade(12,22,belem).
+temperatura_minima(18).
+temperatura_minima(12).
+temperatura_minima(10).
+temperatura_minima(16).
+temperatura_minima(14).
 
+temperatura_maxima(24).
+temperatura_maxima(22).
+temperatura_maxima(20).
+temperatura_maxima(26).
+temperatura_maxima(18).
 
 consulta_temp(Cidade,Tempmin,Tempmax):- clima_sem_umidade(_,Tempmin,Tempmax,_,Cidade).
 
@@ -53,10 +62,9 @@ verifica_umidade(Umidade,Cidade,boa):- clima(_,Umidade,_,_,_,Cidade),Umidade>=50
 
 verifica_umidade(Umidade,Cidade,prejudicial):- clima(_,Umidade,_,_,_,Cidade),Umidade<50.
 
+media_temperatura_cidade(Cidade,Media):- Media is ( ( (temperatura_maxima(Tempmax) ) + (temperatura_minima(Tempmin) ) )/2), 
+					clima_sem_umidade(_,Tempmin,Tempmax,_,Cidade).
 
-
-
-
-
+consulta_umidade_cidade(Cidade,Umidade):- clima(_,95,_,_,_,Cidade).
 
 
